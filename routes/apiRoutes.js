165839dfa008faeee964a74d674f5da31bf3dacb
Agnes Chueng <agneschueng@gmail.com>
2020-11-12 23:04:47 -0600

    get route for /api/workouts to get last workout info

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,15 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 // API Routes
-
+router.get("/api/workouts", (req,res) => {
+    Workout.find({})
+    .sort({ day: 1 })
+    .then(lastWorkout => {
+      res.json(lastWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+})
 
 module.exports = router;
