c67e993909c46b2ca71eeab240538e6958f429b7
Agnes Chueng <agneschueng@gmail.com>
2020-11-12 23:48:18 -0600

    successfully added put request to update workout info

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,29 @@
 const router = require("express").Router();
+const mongojs = require("mongojs");
 const Workout = require("../models/workout.js");
 
 // API Routes
+// Data to main page
 router.get("/api/workouts", (req,res) => {
     Workout.find({})
-    .sort({ day: 1 })
     .then(lastWorkout => {
-      res.json(lastWorkout);
+        res.json(lastWorkout);
     })
     .catch(err => {
-      res.status(400).json(err);
+        res.status(400).json(err);
     });
 })
+
+// Data to exercise page (updating/completing a workout)
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.update({_id:mongojs.ObjectId(req.params.id)}, {$set: req.body})
+    .then(updateWorkout => {
+        res.json(updateWorkout)
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+})
+
 
 module.exports = router;
